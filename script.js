94bc2b4680215947d5b71be7faf745ce5df9b6e2
Felipe Gonçales <felipeggoncales22@gmail.com>
2024-10-05 01:40:03 -0300

    fix scroll

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function bandeja(element) {
         seta.classList.replace('fa-chevron-down', 'fa-chevron-right')
     }
 }
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const sectionId = this.getAttribute('href');
+
+        const section = document.querySelector(sectionId);
+
+        section.scrollIntoView({
+            block: 'center'
+        });
+    });
+});

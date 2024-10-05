da7b9b494fa056b8218ea362c6e81c16d983b3ae
Felipe Gonçales <felipeggoncales22@gmail.com>
2024-10-05 01:21:56 -0300

    fix-css

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const faqs = document.querySelectorAll(".faq");
-
-faqs.forEach((faq) => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    });
-});
+function bandeja(element) {
+    const divText = element.nextElementSibling; 
+    const seta = element.querySelector('i');
+    
+    const divTextDisplay = window.getComputedStyle(divText).display
+    
+    if (divTextDisplay === "none") {
+        divText.style.display = "block";
+        seta.classList.replace('fa-chevron-right', 'fa-chevron-down')
+    } else {
+        divText.style.display = "none";
+        seta.classList.replace('fa-chevron-down', 'fa-chevron-right')
+    }
+}
